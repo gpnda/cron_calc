7b8bb13bf5fb2d28fa5fc5c048a3dc7382c78fc8
Albert <albert@lum.ru>
2024-09-03 18:55:54 +0300

    some design fixes, calculations broken, but better design

--- mycron2.php
+++ mycron2.php
@@ -1,42 +1,25 @@
 <?php
 
-class RES {
-
-    function __construct( ) {
-        $this->value = 0;
-        $this->bump_order = false;
-    }
+class RES {   // ********************************* От этого класса надо избавиться - все в ассоциативный массив
+    public $value = 0;
+    public $bump_order = false;
 }
-
 
 class MYCRON {
 
-  
-
-    public $second = 15;
-    public $minute = 30;
-    public $hour = 17;
-    public $day = 27;
-    public $month = 11;
-    public $year = 2024;
-
-
-
-
-    function increment($x, $max) {
-        $value = 0;
-        $bump_order = false;
-        value = $x + 1;
-        if (value == $max) {
-            value = 0;
-            bump_order = true;
+    private function increment($x, $max) {
+        $result = new RES();
+        $result->value = $x + 1;
+        if ($result->value == $max) {
+            $result->value = 0;
+            $result->bump_order = true;
         }
-        return [$value, $bump_order];
+        return $result;
     }
-    
-    function is_match($filter, $current) {
+
+    private function is_match($filter, $current) {
         $result = false;
-    
+
         if ($filter == "*" || $filter == false) {
             $result = true;
         } elseif (preg_match("/\/\d+/", $filter)) {
@@ -44,7 +27,9 @@ class MYCRON {
             if ($current % $divider == 0) {
                 $result = true;
             }
+
         } elseif (preg_match("/\d+(?:,\d+)+/", $filter)) {
+            
             $str_arr = preg_replace("/\d+(,\d+)+/", "$0", $filter);
             $arr = explode(",", $str_arr);
             $arr = array_map('intval', $arr);
@@ -58,25 +43,36 @@ class MYCRON {
                 $result = true;
             }
         }
-    
+
         return $result;
     }
-    
-    function next_match($filter, $current, $maxvalue) {
+
+    private function next_match($filter, $current, $maxvalue) {
         $tmp_bump_order = false;
         $tmp_res = new RES();
-    
+
         if ($filter == "*" || $filter == false) {
-            $tmp_res = increment($current, $maxvalue);
+            $tmp_res = $this->increment($current, $maxvalue);
             if ($tmp_res->bump_order) {
                 $tmp_bump_order = true;
+            } // ********************************** ТУТ не хватает реализации фильтра - одно значение
+        } elseif (preg_match("/\d+/", $filter)) {
+            $intvalue = intval(preg_replace("/(\d+)/", "$1", $filter));
+            $tmp_res = $this->increment($current, $maxvalue);
+            $x = $tmp_res->value;
+            while ($tmp_res->value != $intvalue) {
+                $tmp_res = $this->increment($x, $maxvalue);
+                if ($tmp_res->bump_order) {
+                    $tmp_bump_order = true;
+                }
+                $x = $tmp_res->value;
             }
         } elseif (preg_match("/\/\d+/", $filter)) {
             $divider = intval(preg_replace("/\/(\d+)/", "$1", $filter));
-            $tmp_res = increment($current, $maxvalue);
+            $tmp_res = $this->increment($current, $maxvalue);
             $x = $tmp_res->value;
             while ($tmp_res->value % $divider != 0) {
-                $tmp_res = increment($x, $maxvalue);
+                $tmp_res = $this->increment($x, $maxvalue);
                 if ($tmp_res->bump_order) {
                     $tmp_bump_order = true;
                 }
@@ -86,11 +82,11 @@ class MYCRON {
             $str_arr = preg_replace("/\d+(,\d+)+/", "$0", $filter);
             $arr = explode(",", $str_arr);
             $arr = array_map('intval', $arr);
-    
-            $tmp_res = increment($current, $maxvalue);
+
+            $tmp_res = $this->increment($current, $maxvalue);
             $x = $tmp_res->value;
             while (!in_array($tmp_res->value, $arr)) {
-                $tmp_res = increment($x, $maxvalue);
+                $tmp_res = $this->increment($x, $maxvalue);
                 if ($tmp_res->bump_order) {
                     $tmp_bump_order = true;
                 }
@@ -99,55 +95,46 @@ class MYCRON {
         } elseif (preg_match("/(\d+)-(\d+)/", $filter)) {
             $fromint = intval(preg_replace("/(\d+)-(\d+)/", "$1", $filter));
             $toint = intval(preg_replace("/(\d+)-(\d+)/", "$2", $filter));
-    
-            $tmp_res = increment($current, $maxvalue);
+
+            $tmp_res = $this->increment($current, $maxvalue);
             $x = $tmp_res->value;
             while (!($tmp_res->value >= $fromint && $tmp_res->value <= $toint)) {
-                $tmp_res = increment($x, $maxvalue);
+                $tmp_res = $this->increment($x, $maxvalue);
                 if ($tmp_res->bump_order) {
                     $tmp_bump_order = true;
                 }
                 $x = $tmp_res->value;
             }
         }
-    
+
         $tmp_res->bump_order = $tmp_bump_order;
         return $tmp_res;
     }
 
-
-
-
-
-
-
-
     public function start_calc() {
-        global $second_filter, $minute_filter, $hour_filter, $day_filter, $month_filter, $year_filter;
-
-        // if (!is_match($second_filter, $this->second)) {
+        
+        // if (!$this::is_match($this->filter['sec'], $this->second)) {
         //     $this->get_next_second();
         // }
-        if (!is_match($minute_filter, $this->minute)) {
+        if (!$this->is_match($this->filter['min'], $this->minute)) {
             $this->get_next_minute();
         }
-        if (!is_match($hour_filter, $this->hour)) {
+        if (!$this->is_match($this->filter['hour'], $this->hour)) {
             $this->get_next_hour();
         }
-        if (!is_match($day_filter, $this->day)) {
+        if (!$this->is_match($this->filter['day'], $this->day)) {
             $this->get_next_day();
         }
-        if (!is_match($month_filter, $this->month)) {
+        if (!$this->is_match($this->filter['mon'], $this->month)) {
             $this->get_next_month();
         }
-        if (!is_match($year_filter, $this->year)) {
+        if (!$this->is_match($this->filter['year'], $this->year)) {
             $this->get_next_year();
         }
     }
 
     public function get_next_second() {
-        global $second_filter;
-        $tmp_res = next_match($second_filter, $this->second, 60);
+        $tmp_res = $this->next_match($this->filter['sec'], $this->second, 60);
         $this->second = $tmp_res->value;
         if ($tmp_res->bump_order) {
             $this->get_next_minute();
@@ -155,8 +142,7 @@ class MYCRON {
     }
 
     public function get_next_minute() {
-        global $minute_filter;
-        $tmp_res = next_match($minute_filter, $this->minute, 60);
+        $tmp_res = $this->next_match($this->filter['min'], $this->minute, 60);
         $this->minute = $tmp_res->value;
         if ($tmp_res->bump_order) {
             $this->get_next_hour();
@@ -164,8 +150,7 @@ class MYCRON {
     }
 
     public function get_next_hour() {
-        global $hour_filter;
-        $tmp_res = next_match($hour_filter, $this->hour, 24);
+        $tmp_res = $this->next_match($this->filter['hour'], $this->hour, 24);
         $this->hour = $tmp_res->value;
         if ($tmp_res->bump_order) {
             $this->get_next_day();
@@ -173,8 +158,7 @@ class MYCRON {
     }
 
     public function get_next_day() {
-        global $day_filter;
-        $tmp_res = next_match($day_filter, $this->day, 30);
+        $tmp_res = $this->next_match($this->filter['day'], $this->day, 30);
         $this->day = $tmp_res->value;
         if ($tmp_res->bump_order) {
             $this->get_next_month();
@@ -182,8 +166,7 @@ class MYCRON {
     }
 
     public function get_next_month() {
-        global $month_filter;
-        $tmp_res = next_match($month_filter, $this->month, 12);
+        $tmp_res = $this->next_match($this->filter['mon'], $this->month, 12);
         $this->month = $tmp_res->value;
         if ($tmp_res->bump_order) {
             $this->get_next_year();
@@ -191,40 +174,83 @@ class MYCRON {
     }
 
     public function get_next_year() {
-        global $year_filter;
-        $tmp_res = next_match($year_filter, $this->year, 10000);
+        $tmp_res = $this->next_match($this->filter['year'], $this->year, 10000);
         $this->year = $tmp_res->value;
+    }
+
+
+
+    public function nextTime($filter, $currenttime) {
+
+
+        // ************ Здесь эти переменные парсим из строковой перменной $currenttime **********************
+        $this->second = 15;
+        $this->minute = 30;
+        $this->hour = 17;
+        $this->day = 27;
+        $this->month = 11;
+        $this->year = 2024;
+
+        // Это чтоб все не-указанные фильтры по умолчанию были '*'
+        $this->filter = array_merge(
+            [
+            'sec'=>'*', 
+            'min'=>'*', 
+            'hour'=>'*', 
+            'day'=>'*', 
+            'mon'=>'*', 
+            'year'=>'*' 
+            ], 
+            $filter
+        );
+
+        // Делаем сдвиг всех первых значений кроме секунды (чувствуется что криво, видимо сама эта логика содержит ошибку)
+        $this->start_calc();
+
+        // Основной запуск, он каскадом спустится от секунд до года.
+        $this->get_next_second();
+        $resultstr = $this->hour . ":" . $this->minute . ":" . $this->second . "   " . $this->day . "-" . $this->month . "-" . $this->year . "\n";
+        
+        // Дернем для проверки еще несколько раз, просто чтоб вывести на экран. Потом надо убрать **********************************
+        echo $resultstr;
+        for ($i=0;$i<20 ; $i++) {
+            $this->get_next_second();
+            echo $this->hour . ":" . $this->minute . ":" . $this->second . "   " . $this->day . "-" . $this->month . "-" . $this->year . "\n";
+        }
+
+        return $resultstr;
+    }
+
+
+
+}
+
+
+
+
+
+
+
+// Декоратор
+class CronTimer {
+    public static function nextTime($filter, $currenttime) {
+        return (new MYCRON())->nextTime($filter, $currenttime);
     }
 }
 
 
+$start_date = "01.01.2024 10:00:00";
 
+$filter = [
+    "sec" => "1-5",		// Любое число секунд
+    "min" => "3,4,5",	// Число минут кратное 10
+    "hour" => "*",	// Час от 9 до 18 включительно
+    "day" => "*",		// День любой
+    "mon" => "*",		// Месяц любой
+    "year" => "*"		// Год любой
+];
 
-
-
-
-
-echo "Start1\n";
-
-$now_second = 15;
-$now_minute = 30;
-$now_hour = 17;
-$now_day = 27;
-$now_month = 11;
-$now_year = 2024;
-
-$second_filter = "1-5";
-$minute_filter = "/7";
-$hour_filter = "/5";
-$day_filter = "9-18";
-$month_filter = "9-18";
-$year_filter = "9-18";
-
-$mc = new MYCRON();
-$mc->start_calc();
-while (true) {
-    $mc->get_next_second();
-    echo $mc->hour . ":" . $mc->minute . ":" . $mc->second . "\n";
-}
+// Здесь выводим искомое значение
+echo CronTimer::nextTime($filter, $start_date);
 
 ?>
